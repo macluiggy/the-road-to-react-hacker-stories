3b939ee4c228a96f9eb42b3336c6117582e611b8
Luiggy Macias <ferrinluiggy@gmail.com>
2021-06-30 09:47:30 -0500

    bringing data

--- src/API.js
+++ src/API.js
@@ -1,19 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
 const API = () => {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState('posts');
+    const [value2, setValue2] = useState([])
     useEffect(() => {
-
-    },)
+        bringData()
+    }, [value2, setValue2])
 
     const bringData = async () => {
-    	await fetch('')
+        let data = await fetch('https://jsonplaceholder.typicode.com/' + value)
+        let res = await data.json()
+        setValue2(res)
     }
+
+
     return (
         <div>
-			
+			<Input
+			value='posts'
+			id='one'
+			setValue={() => setValue('posts')} />
+			<Input
+			value='comments'
+			id='two'
+			setValue={() => setValue('comments')} />
+
+			<ul>
+				{value2.map(({id, body})=> <li key={id}>{body}</li>)}
+			</ul>
 		</div>
     )
 }
 
+const Input = ({ id, type = 'button', value = 'button', setValue }) =>
+    <>
+	    <label htmlFor={id}></label> 
+	    <input
+			type = { type }
+			value = { value }
+			id = { id }
+			onClick = { setValue } >
+	    </input> 
+    </>
 export default API
