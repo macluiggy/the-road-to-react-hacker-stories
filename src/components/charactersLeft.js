ca6d418b43d27e64b6242ed717d17877e1b1eff4
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-01 11:35:09 -0500

    add a comment with a warning if the number of character exceed

--- src/components/charactersLeft.js
+++ src/components/charactersLeft.js
@@ -1,15 +1,37 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 
 
 const CharactersLeft = () => {
-	 const [text, setText] = useState('')
+	const [text, setText] = useState('')
+	const [style, setStyle] = useState()
+	const [warning, setWarning] = useState('')
 	let textareaRef = useRef()
+	let ulRef = useRef()
 	const updateCharactersLeft = () => {
-		console.log(textareaRef.current.value)
+		//console.log(textareaRef.current.value)
 		setText(textareaRef.current.value)
-	}
 
-	let leftNumberOfCharacters = 10 - text.length
+	}
+	const addText = () => {
+		console.log(textareaRef.current.value === text)
+		if (text.length > 10) {
+			setWarning('number of characters exceeded')
+			console.log('se excedio')
+			return 
+		}
+		setWarning('')
+		ulRef.current.innerHTML += `<li>${text}</li>`;
+	}
+	useEffect(() => {
+		setStyle({
+			 	border: 10-text.length < 0 ? 'solid red' : 'solid green',
+			 	outline: 'none',
+			 	position: 'relative',
+			 	left: `${text.length*10}px`
+			 })
+		//console.log('se renderizo')
+	}, [text, setText])
+	
 	return (
 		<div>
 			<textarea
@@ -19,25 +41,27 @@ const CharactersLeft = () => {
 			 rows="10"
 			 ref={textareaRef}
 			 onInput={updateCharactersLeft}
-			 style={{
-			 	border: leftNumberOfCharacters < 0 ? 'solid red' : 'solid green',
-			 	outline: 'none',
-			 	position: 'relative',
-			 	left: `${text.length*10}px`
-			 }} ></textarea>
-			<p>{leftNumberOfCharacters} left</p>
+			 style={style} ></textarea>
+			<p>{10-text.length} left</p><p>{warning}</p>
+			<Input
+			type='submit'
+			value='submit'
+			addText={addText} />
+			<ul ref={ulRef}>
+				
+			</ul>
 		</div>
 		)
 }
 
-const Input = ({id, type='button', value='button', changeColor}) => 
+const Input = ({id, type='button', value='button', addText}) => 
 	<>
 		<label htmlFor={id}></label>
 		<input 
 		type={type} 
 		value={value} 
 		id={id}
-		onClick={changeColor} >
+		onClick={addText} >
 		
 		</input>
 	</>
