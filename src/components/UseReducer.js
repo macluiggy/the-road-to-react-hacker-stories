ebca40f4203770e585151a0c0ea0f21c6e717e33
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-09 11:31:07 -0500

    practicing usereducer hook

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -6,18 +6,41 @@ const reducer = (state, action) => {
 	: action.type === 'decrease'
 	? state - 1
 	: 0*/
-	switch (action.type) {
-    case 'INCREASE':
-      return { ...state, n: state.n + 1 };
-    case 'DECREASE':
-      return { ...state, n: state.n - 1 };
-    default:
-      return state;
-  }
+	switch(action.type) {
+		case 'increase':
+			return state + 1;
+		case 'decrease':
+		  	return state - 1;
+	 	default:
+	 		return 0
+	}
 }
+const initialState = {
+  firstname: 'Liesa',
+  lastname: 'Huppertz',
+  age: 30,
+};
+ 
+const action = {
+  type: 'INCREASE_AGE',
+  payload: {
+  	lastname: 'Wieruch',
+  },
+};
 
+const personReducer = (person, action) => {
+	switch (action.type) {
+		case 'INCREASE_AGE':
+			return {...person, age: person.age + 1};
+		case 'CHANGE_LASTNAME':
+			return {...person, lastname: action.payload.lastname};
+		default:
+			return person;
+	}
+}
+const result = personReducer(initialState, action)
 const  UseReducer = () => {
-	const [count, dispatchCount] = useReducer(reducer, {comment: 'hola', n: 10});
+	const [count, dispatchCount] = useReducer(reducer, 0);
 	useEffect(() => {
 		console.log('rendered')
 	}, [count])
@@ -30,9 +53,80 @@ const  UseReducer = () => {
 			decrease
 		</button>
 		<button onClick={() => dispatchCount({type: 'reset'})} >reset</button> <br />
-			{count.n}
+			{count}
+			<p>{result.firstname} {result.lastname} - age: {result.age} </p>
+
+		<Todos />
 		</div>
 		)
+}
+
+ 
+const initialTodos = [
+  {
+    id: 'a',
+    task: 'Learn React',
+    complete: false,
+  },
+  {
+    id: 'b',
+    task: 'Learn Firebase',
+    complete: false,
+  },
+];
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'DO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: true };
+        } else {
+          return todo;
+        }
+      });
+    case 'UNDO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: false };
+        } else {
+          return todo;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+const Todos = () => {
+	const [todos, dispatch] = React.useReducer(
+    todoReducer,
+    initialTodos
+  );
+
+	const handleChange = todo => {
+		dispatch({
+			type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
+			id: todo.id
+		})
+	};
+ 
+	  return (
+	    <ul>
+	      {todos.map(todo => (
+	        <li key={todo.id}>
+	          <label>
+	            <input
+	              type="checkbox"
+	              checked={todo.complete}
+	              onChange={() => handleChange(todo)}
+	            />
+	            {todo.task}
+	          </label>
+	        </li>
+	      ))}
+	    </ul>
+	  );
 }
 
 export default UseReducer
